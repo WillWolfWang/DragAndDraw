e0dd4ea01be531b5e1b2a1b58b496bb940e5d932
wangzhenhua <WillWolf_Wang@126.com>
2024-10-03 15:11:39 +0800

    添加一个太阳上升的属性动画

--- app/src/main/java/com/will/draganddraw/SunActivity.kt
+++ app/src/main/java/com/will/draganddraw/SunActivity.kt
@@ -1,9 +1,13 @@
 package com.will.draganddraw
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.AnimationSet
@@ -27,6 +31,7 @@ class SunActivity: AppCompatActivity() {
         ContextCompat.getColor(this, R.color.night_sky)
     }
 
+    private var isSunDown = false;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sun)
@@ -36,7 +41,14 @@ class SunActivity: AppCompatActivity() {
         skyView = findViewById(R.id.sky)
 
         sceneView.setOnClickListener {
-            startAnimation()
+            if (isSunDown) {
+                sunupAnimator()
+                isSunDown = false
+            } else {
+                startAnimation()
+                isSunDown = true
+            }
+
         }
     }
 
@@ -47,7 +59,34 @@ class SunActivity: AppCompatActivity() {
         val heightAnimator = ObjectAnimator.ofFloat(sunView, "y", sunYStart, sunYEnd)
             .setDuration(3000)
 
+        val scaleXAnimator = ObjectAnimator.ofFloat(sunView, View.SCALE_X, 1f, 1.5f)
+            .setDuration(1000)
+        val scaleYAnimator = ObjectAnimator.ofFloat(sunView, View.SCALE_Y, 1f, 1.5f)
+            .setDuration(1000)
+        scaleXAnimator.repeatCount = -1
+        scaleXAnimator.repeatMode = ValueAnimator.REVERSE
+        scaleYAnimator.repeatCount = -1
+        scaleYAnimator.repeatMode = ValueAnimator.REVERSE
+
+
         heightAnimator.interpolator = AccelerateInterpolator()
+
+        heightAnimator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+                Log.e("WillWolf", "onAnimationEnd-->")
+                scaleXAnimator.cancel()
+                scaleYAnimator.cancel()
+                if (sunView.scaleX != 1f) {
+                    scaleXAnimator.repeatCount = 0
+                    scaleXAnimator.reverse()
+                }
+                if (sunView.scaleY != 1f) {
+                    scaleYAnimator.repeatCount = 0
+                    scaleYAnimator.reverse()
+                }
+            }
+        })
 
         val sunsetSkyAnimator = ObjectAnimator.ofInt(skyView, "backgroundColor", blueSkyColor, sunsetSkyColor)
             .setDuration(3000)
@@ -58,14 +97,39 @@ class SunActivity: AppCompatActivity() {
         nightSkyAnimator.setEvaluator(ArgbEvaluator())
 
 
-//        heightAnimator.start()
-//        skyAnimator.start()
-
         // AnimatorSet 是可以放在一起执行的动画集
         val animatorSet = AnimatorSet()
         animatorSet.play(heightAnimator)
             .with(sunsetSkyAnimator)
+            .with(scaleXAnimator)
+            .with(scaleYAnimator)
             .before(nightSkyAnimator)
+
+        animatorSet.start()
+    }
+
+    private fun sunupAnimator() {
+        val sunYStart = skyView.height.toFloat()
+        val sunYEnd = sunView.top.toFloat()
+
+
+        val heightAnimator = ObjectAnimator.ofFloat(sunView, "y", sunYStart, sunYEnd)
+            .setDuration(3000)
+
+        heightAnimator.interpolator = AccelerateInterpolator()
+
+        val sunsetSkyAnimator = ObjectAnimator.ofInt(skyView, "backgroundColor", sunsetSkyColor, blueSkyColor)
+            .setDuration(3000)
+        sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
+
+        val nightSkyAnimator = ObjectAnimator.ofInt(skyView, "backgroundColor", nightSkyColor, sunsetSkyColor,)
+            .setDuration(1500)
+        nightSkyAnimator.setEvaluator(ArgbEvaluator())
+
+        val animatorSet = AnimatorSet()
+        animatorSet.play(heightAnimator)
+            .with(sunsetSkyAnimator)
+            .after(nightSkyAnimator)
         animatorSet.start()
     }
 }
