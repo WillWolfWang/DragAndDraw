560fa4c8c146c83308559f6776dbfd736ad424da
wangzhenhua <WillWolf_Wang@126.com>
2024-10-03 11:47:07 +0800

    初始化项目

---
+++ app/src/main/java/com/will/draganddraw/BoxDrawingView.kt
@@ -0,0 +1,8 @@
+package com.will.draganddraw
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class BoxDrawingView(context: Context, attrs: AttributeSet? = null): View(context, attrs) {
+}
