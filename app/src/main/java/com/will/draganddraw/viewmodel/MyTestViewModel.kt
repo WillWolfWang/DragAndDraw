dd307ec652d6cddf659059644698b974dd573096
wangzhenhua <WillWolf_Wang@126.com>
2024-10-05 15:30:03 +0800

    练习 viewModel ,将 ViewModel 中的 MutableLiveData 变成 private 不可见，对外提供 LiveData 类型，只能读，不能修改

--- app/src/main/java/com/will/draganddraw/viewmodel/MyTestViewModel.kt
+++ app/src/main/java/com/will/draganddraw/viewmodel/MyTestViewModel.kt
@@ -1,5 +1,6 @@
 package com.will.draganddraw.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlin.concurrent.thread
@@ -22,20 +23,22 @@ class MyTestViewModel(countReserved: Int): ViewModel() {
      * 使用 LiveData，可以通过订阅的方式，当 LiveData 数据更新
      * 然后通知订阅者
      */
-    var counter = MutableLiveData<Int>()
+    private var _counter = MutableLiveData<Int>()
+    val counter: LiveData<Int>
+        get() = _counter
     // 为什么有 init 方法，因为 kotlin 没有办法
     // 在主构造器中进行一些初始化操作，所以加了一个 init
     init {
-        counter.value = countReserved
+        _counter.value = countReserved
     }
 
     fun plusOne() {
         thread {
             // 模拟耗时
             Thread.sleep(2000)
-            val count = counter.value ?: 0
+            val count = _counter.value ?: 0
             // 如果在子线程中，需要使用 postValue 方法
-            counter.postValue( count + 1)
+            _counter.postValue( count + 1)
 //            counter.value =
         }
 
