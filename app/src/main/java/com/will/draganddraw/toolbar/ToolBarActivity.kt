03f22ce75b88bf057a108cbdb2e676c732f4ea3c
wangzhenhua <WillWolf_Wang@126.com>
2024-10-05 10:32:42 +0800

    练习 draw layout

--- app/src/main/java/com/will/draganddraw/toolbar/ToolBarActivity.kt
+++ app/src/main/java/com/will/draganddraw/toolbar/ToolBarActivity.kt
@@ -5,6 +5,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.will.draganddraw.R
 import com.will.draganddraw.databinding.ActivityToolBarBinding
 
@@ -16,6 +17,13 @@ class ToolBarActivity: AppCompatActivity() {
         setContentView(viewBinding.root)
         // 启用 toolbar
         setSupportActionBar(viewBinding.toolbar)
+        // toolbar 与 draw layout 关联
+        supportActionBar?.let {
+            // 这里是让 toolbar 最左侧的图标显示出来，一般是一个向左的箭头图片，意思是返回上一级
+            it.setDisplayHomeAsUpEnabled(true)
+            // 修改图片为菜单形状
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
     }
 
     // Toolbar 中的 action 按钮只会显示图标，菜单中的 action 按钮只会显示文字
@@ -26,6 +34,8 @@ class ToolBarActivity: AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
+            // home 菜单的 id 固定是  android.R.id.home ，打开抽屉菜单
+            android.R.id.home -> viewBinding.drawerLayout.openDrawer(GravityCompat.START)
             R.id.backup -> Toast.makeText(this, "backup", Toast.LENGTH_SHORT).show()
             R.id.delete -> Toast.makeText(this, "delete", Toast.LENGTH_SHORT).show()
             R.id.settings -> Toast.makeText(this, "setting", Toast.LENGTH_SHORT).show()
